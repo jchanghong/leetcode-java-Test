5531d0e4951a2320664bec38bf181f689e2c1a03
jchanghong <3200601@qq.com>
2017-08-15 16:48:22 +0800

    代码全部写到test目录

--- src/test/java/_002_AddTwoNumbers/Practice.java
+++ src/test/java/_002_AddTwoNumbers/Practice.java
@@ -21,7 +21,72 @@ public class Practice {
 
     public ListNode addTwoNumbers(ListNode a, ListNode b) {
         // TODO Auto-generated method stub
-        return null;
+        ListNode head = new ListNode(0);
+        ListNode pre = head;
+        boolean big10 = false;
+        while (a != null && b != null) {
+            int sum = a.val + b.val;
+            if (big10) {
+                sum++;
+            }
+            ListNode listNode = new ListNode(0);
+            if (sum < 10) {
+                listNode.val = sum;
+                big10 = false;
+            }
+            else {
+                sum = sum - 10;
+                listNode.val = sum;
+                big10 = true;
+            }
+            pre.next = listNode;
+            pre = listNode;
+            a = a.next;
+            b = b.next;
+
+        }
+        while (a != null) {
+            int sum = a.val;
+            if (big10) {
+                sum++;
+            }
+            ListNode listNode = new ListNode(0);
+            if (sum < 10) {
+                listNode.val = sum;
+                big10 = false;
+            }
+            else {
+                sum = sum - 10;
+                listNode.val = sum;
+                big10 = true;
+            }
+            pre.next = listNode;
+            pre = listNode;
+            a = a.next;
+        }
+        while (b != null) {
+            int sum = b.val;
+            if (big10) {
+                sum++;
+            }
+            ListNode listNode = new ListNode(0);
+            if (sum < 10) {
+                listNode.val = sum;
+                big10 = false;
+            }
+            else {
+                sum = sum - 10;
+                listNode.val = sum;
+                big10 = true;
+            }
+            pre.next = listNode;
+            pre = listNode;
+            b = b.next;
+        }
+        if (big10) {
+            pre.next = new ListNode(1);
+        }
+        return head.next;
     }
 
 }
